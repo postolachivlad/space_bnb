4913031b0ac50634200a4422dc55a613675ffe42
Vladislav Postolachi <postolachi.vlad@gmail.com>
2020-01-09 20:35:53 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+
+  resources :spaceships do
+    resources :reviews, only: [:new, :create]
+  end
 end
