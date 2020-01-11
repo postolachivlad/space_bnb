ee561df86c3cef529e4ae63a4e90061c5e47c0be
Dan Kreibich <dan@lh-innovationhub.com>
2020-01-11 16:30:19 +0100

    added reviews funcionality -> Still needs adjustments in the frontend

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,34 @@
 class ReviewsController < ApplicationController
+
+  def index
+    set_spaceship
+    @reviews = Review.all
+  end
+
+  def new
+    set_spaceship
+    authorize @ship
+    @review = Review.new
+    authorize @review
+  end
+
+  def create
+    @review = Review.new(review_params)
+    set_spaceship
+    @review.spaceship = @ship
+    authorize @review
+    @review.save
+    redirect_to spaceship_path(@ship)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+
+  def set_spaceship
+    @ship = Spaceship.find(params[:spaceship_id])
+  end
+
 end
