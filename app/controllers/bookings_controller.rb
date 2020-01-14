38a2a3c06af278184eb7fa793af08a5bf4229087
Vladislav Postolachi <postolachi.vlad@gmail.com>
2020-01-14 17:01:14 +0100

    Creat booking controller, pundit-policy file, styles

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,35 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = policy_scope(Booking)
+  end
+
+  def new
+    @ship = Spaceship.find(params[:spaceship_id])
+    @booking = Booking.new
+    authorize @booking
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @ship = Spaceship.find(params[:spaceship_id])
+    @booking.spaceship_id = @ship.id
+    @booking.user_id = @ship.user_id
+    # check if actions is working after finish the view form
+
+    if @booking.save
+      #redirect user(@spaceship)
+    else
+      render :new
+    end
+    authorize @booking
+  end
+
+  # discus with teammates if we will need other actions for this controller
+
+  private
+
+  def booking_params
+    params.required(:booking).permit(:start_date, :end_date, :user_id, :spaceship_id, :created_at, :updated_at)
+  end
 end
