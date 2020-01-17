26d8703c3bcc80c788690c53963e6b36e0bdf683
Vladislav Postolachi <postolachi.vlad@gmail.com>
2020-01-17 17:25:56 +0100

    Fixed a bug on index page, now when you clic on the 'I need a spaceship' we will load all the spaceships and if we are serching for something in search bar we willshow the result of this searching

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -5,8 +5,10 @@ class SpaceshipsController < ApplicationController
   def index
     # find all Spaceship from DB and initialize them in @ship instance
     # raise
-    @ships = policy_scope(Spaceship)
-    @ships = Spaceship.search_ships(params[:query])
+    @ships = policy_scope(Spaceship)\
+
+    # diferent name for instance with reason to not over write it
+    @search_ships = Spaceship.search_ships(params[:query])
 
     # authorize @ships
 
