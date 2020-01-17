94edc50b991c9ee95925e8cbe6c3e942b024eb18
Vladislav Postolachi <postolachi.vlad@gmail.com>
2020-01-17 16:12:30 +0100

    add validation for rating from1-5, set the input form to receive numbers from 1-5

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :spaceship
   validates  :description, presence: true
-  validates  :rating, presence: true
+  validates  :rating, presence: true, inclusion: 1..5
 end
